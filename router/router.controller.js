bbce221cf0d72e67512d9dd17b1e6ec870bee99a
siva kumar <58546115+sivakumar1912@users.noreply.github.com>
2020-01-06 13:06:32 +0530

    Update router.controller.js

--- router/router.controller.js
+++ router/router.controller.js
@@ -1,1 +1,8 @@
+const express = require('express');
+const app = express();
+const orderRoute = require('./order.router');
+const ProductRoute = require('./product.router');
+app.use('/',productRoute);
+app.use('/order',orderRoute);
 
+module.exports = app;
