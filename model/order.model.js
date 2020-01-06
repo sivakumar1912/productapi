93d38c9989656abdc0a38b6fed0354c7366f5a1e
siva kumar <58546115+sivakumar1912@users.noreply.github.com>
2020-01-06 13:17:33 +0530

    Update order.model.js

--- model/order.model.js
+++ model/order.model.js
@@ -1,1 +1,24 @@
+// db schema
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// declaring collection schema property and names
+
+let Order = new Schema(
+  {
+    // schema property: values
+    username: { type: String },
+    address: { type: String },
+    mobile: { type: Number },
+    orderinfo: { type: String },
+    orderstatus: { type: String },
+    orderdate: { type:Date }
+  },
+  {
+    collection: 'order'
+  }
+);
+
+module.exports = mongoose.model('Order', Order);
 
